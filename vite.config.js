dd0d90ffda4b8e74b7bab7b674e9dea9ad51d801
k! <dvrchipro+klwpgk@gmail.com>
2025-12-27 02:29:27 +0100

    les anciens a efacer

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [react()],
   optimizeDeps: {
     exclude: ['js-big-decimal']
   }
