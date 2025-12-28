a7c9405a3673bca3d92c6851647d95dd80ab4898
k! <dvrchipro+klwpgk@gmail.com>
2025-12-28 03:22:35 +0100

    🚀 Setup GitHub Pages deployment via GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mariok3/',
   plugins: [
     svelte({
       preprocess: vitePreprocess()
